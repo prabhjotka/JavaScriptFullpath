c1b8327e4ba73a6660c58cac342dc118830594be
Prabh <sonukaur68@gmail.com>
2024-01-09 14:38:11 -0700

    variable

---
+++ 01_variables.js
@@ -0,0 +1,9 @@
+const accountId=3535;
+let accountemail="sam@gmail.com";
+accountCity='Calgary';
+
+console.log(accountId);
+// accountId=5656;     // Not allowed as const
+console.log(accountemail);
+
+console.table([accountId,accountemail,accountCity]);
